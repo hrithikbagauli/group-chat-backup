ea6a506623750a10db3c2ba9ae285de5f1c2abb7
Hrithik Bagauli <hrithikbagauli@gmail.com>
2023-01-24 18:09:42 +0530

    Updating messages right away

--- back-end/controllers/message.js
+++ back-end/controllers/message.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const Message = require('../models/message');
 const User = require('../models/user');
+const {Op} = require('sequelize');
+
 exports.postSendMessage = async (req, res, next) => {
     try {
         await req.user.createMessage({ message: req.body.message });
@@ -12,8 +14,18 @@ exports.postSendMessage = async (req, res, next) => {
 }
 
 exports.getMessages = async (req, res, next) => {
+    const last_message_id = 0;
+    if(last_message_id){
+        last_message_id = req.query.id;
+    }
+    // console.log('00000000000000000000000000000000000000000000000000000000',last_message_id);
     try {
         const messages = await Message.findAll({
+            where: {
+                id: {
+                   [Op.gt]: last_message_id
+                }
+            },
             include: { model: User, attributes: ['name', 'createdAt'] }
         });
         res.json(messages);
--- front-end/js/chat.js
+++ front-end/js/chat.js
@@ -1,12 +1,13 @@
 const myform = document.getElementById('myform');
 const joined_div = document.getElementById('joined');
 const message_input = document.querySelector('.message');
-const message_div = document.getElementById('message_div'); 
+const message_div = document.getElementById('message_div');
 let token = localStorage.getItem('token');
 
 document.addEventListener('DOMContentLoaded', function (e) {
     setInterval(async () => {
         try {
+            getMessages();
             const users = await axios.get('http://localhost:4000/online-user');
             joined_div.replaceChildren();
             for (let i = 0; i < users.data.length; i++) {
@@ -20,38 +21,40 @@ document.addEventListener('DOMContentLoaded', function (e) {
             console.log(err)
         }
     }, 1000);
-
-    getMessages();
 })
 
 myform.addEventListener('submit', async function (e) {
     e.preventDefault();
     try {
-        axios.post('http://localhost:4000/send-message', { message: message_input.value }, { headers: { Authorization: token } })
+        if (message_input.value != '') {
+            await axios.post('http://localhost:4000/send-message', { message: message_input.value }, { headers: { Authorization: token } })
+            getMessages();
+            message_input.value = '';
+        }
     } catch (err) {
         console.log(err);
     }
 })
 
-async function getMessages(){
+async function getMessages() {
     try {
-        const messages = await axios.get('http://localhost:4000/get-messages', { headers: { Authorization: token } });
+        const messages = await axios.get(`http://localhost:4000/get-messages`, { headers: { Authorization: token } });
         message_div.replaceChildren();
-        for(let i=0; i<messages.data.length; i++){
+        for (let i = 0; i < messages.data.length; i++) {
             const div = document.createElement('div');
             let time = convertTime(messages.data[i].createdAt);
-            if(messages.data[i].user.name == localStorage.getItem('username')){
+            if (messages.data[i].user.name == localStorage.getItem('username')) {
                 div.classList.add('d-flex', 'p-0', 'w-100', 'justify-content-end', 'pe-2')
-                div.innerHTML = 
-                `<span class="wrap bg-primary text-white my-2 pb-0 rounded">
+                div.innerHTML =
+                    `<span class="wrap bg-primary text-white my-2 pb-0 rounded">
                 ${messages.data[i].message}
                 <div class="p-0 m-0 d-flex justify-content-end"><span class="p-0 m-0 fw-bold">${time}</span></div>
                 </span>`
             }
-            else{
+            else {
                 div.classList.add('d-flex', 'justify-content-start', 'p-0', 'w-100', 'mt-2');
-                div.innerHTML = 
-                `<span class="wrap bg-light rounded pb-0">
+                div.innerHTML =
+                    `<span class="wrap bg-light rounded pb-0">
                 <div class="p-0 fw-bold"><span class="p-0">${messages.data[i].user.name}</span></div>
                 ${messages.data[i].message} 
                 <div class="p-0 m-0 d-flex justify-content-end"><span class="p-0 m-0 fw-bold">${time}</span></div>
@@ -64,15 +67,16 @@ async function getMessages(){
     }
 }
 
-function convertTime(time){
+function convertTime(time) {
     let time_indicator = 'am';
-    let hour = parseInt(time.slice(11,13));
-    if(hour>=12 && hour<24){
+    let hour = parseInt(time.slice(11, 13));
+    if (hour >= 12 && hour < 24) {
         time_indicator = 'pm';
-        if(hour>12){
-            hour = hour-12;
+        if (hour > 12) {
+            hour = hour - 12;
         }
     }
     const minutes = time.slice(13, 16);
     return `${hour}${minutes} ${time_indicator}`;
 }
+
